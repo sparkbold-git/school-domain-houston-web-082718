e261b4649910b51fcc57ae8cf4124c2952abfb2a
Trung Hoang <sparkboldstudio@gmail.com>
2018-08-31 08:41:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+
+  end
+  
+  def add_student(student,grade)
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+      @roster[grade] << student
+  end
+  
+  
+  def grade(num)
+    @roster[num]
+  end
+  
+  def sort
+   @roster.each do |grade,students|
+     @roster[grade] = students.sort
+   end
+  end
+end
